1c0661bf0180f42f9312086d2c65f998949c09cc
alperendiler <alperendiler1@gmail.com>
2022-06-21 08:25:39 +0300

    Finish

---
+++ app/src/main/java/com/alperendiler/spotify2/SpotifyApplication.kt
@@ -0,0 +1,7 @@
+package com.alperendiler.spotify2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SpotifyApplication : Application()
---
+++ app/src/main/java/com/alperendiler/spotify2/data/remote/MusicDatabase.kt
@@ -0,0 +1,21 @@
+package com.alperendiler.spotify2.data.remote
+
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
+import com.alperendiler.spotify2.data.entities.Song
+import com.alperendiler.spotify2.other.Constants.SONG_COLLECTION
+import kotlinx.coroutines.tasks.await
+
+class MusicDatabase {
+
+    private val firestore = FirebaseFirestore.getInstance()
+    private val songCollection = firestore.collection(SONG_COLLECTION)
+
+    suspend fun getAllSongs(): List<Song> {
+        return try {
+            songCollection.get().await().toObjects(Song::class.java)
+        } catch(e: Exception) {
+            emptyList()
+        }
+    }
+}
